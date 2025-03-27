9342f7c0a91dce711248622a2568043f5e4842b1
Alessandro Virmond <avirmond2000@gmail.com>
2025-03-27 16:27:06 -0300

    rotas criadas com aproveitamento de componentes basicos

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Router from './routes'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <Router/>
   </React.StrictMode>
 );
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,8 +1,26 @@
+import cardapio from "data/cardapio.json";
+import styles from './Inicio.module.scss'
+
 export default function Inicio() {
+  let pratosRecomedados = [...cardapio];
+  pratosRecomedados = pratosRecomedados.sort(() => 0.5 - Math.random()).splice(0,3)
   return (
     <section>
-      <h3></h3>
-      <div></div>
+      <h3 className={styles.titulo}
+      >Recomendações da cozinha</h3>
+      <div className={styles.recomendados} >
+        {pratosRecomedados.map((i) => 
+          <div key={i.id} className={styles.recomendado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={i.photo} alt={i.title}></img>
+            </div>
+            <button className={styles.recomendado__botao}>
+              Ver mais
+            </button>
+          </div>
+
+        )}
+      </div>
     </section>
   );
 }
