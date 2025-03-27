f831b93f8d714c0c152d4888913ca4fc66ea4481
Alessandro Virmond <avirmond2000@gmail.com>
2025-03-27 13:18:10 -0300

    estrutura inicial feita

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import Cardapio from 'pages/Cardapio';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Cardapio />
+  </React.StrictMode>
+);
---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,8 @@
+export default function Inicio() {
+  return (
+    <section>
+      <h3></h3>
+      <div></div>
+    </section>
+  );
+}
